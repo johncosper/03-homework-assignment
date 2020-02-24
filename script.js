94f5fdae3d9cbf18114f19da371f59642a35f474
John Cosper <Johngcosper@gmail.com>
2020-02-23 19:15:55 -0500

    finished prompts and confirms to set criteria for password

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ btn.addEventListener("click", function() {
             alert("Sorry, your password needs to be between 8 and 128 characters!");
         }
         else {
-            alert("Great we will make your password " + pwlength + " characters long!")
+            alert("Great we will make your password " + pwlength + " characters long!");
         }
         while (pwlength < 8) {
             var pwlength = prompt("How long would you like your Password?", "between 8-128 characters");
@@ -20,11 +20,31 @@ btn.addEventListener("click", function() {
     var spechar = confirm("Would you like to include special characters in your password?");
 
         if (spechar == true) {
-            alert("Okay! We'll include special characters!")
+            alert("Okay! We'll include special characters!");
         }
         else {
-            alert("Okay! We won't include any special characters!")
+            alert("Okay! We won't include any special characters!");
         }
+    
+    var numbers = confirm("Would you like to include numbers in your password?");
+
+        if (numbers == true) {
+            alert("Okay! We'll include numbers!");
+        }
+        else {
+            alert("Okay! We won't include any numbers!");
+        }
+
+    var uppercase = confirm("Would you like to include uppercase letters in your password?");
+
+        if (uppercase == true) {
+            alert("Okay! We'll include uppercase letters!");
+        }
+        else {
+            alert("Okay! We won't include any uppercase letters!");
+        }
+    
+    alert("Are you ready for your brand new password? Because here it comes!")
 
         
 });
