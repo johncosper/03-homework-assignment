9ed07b10819a677ab86f2d94910986e5e344af41
John Cosper <Johngcosper@gmail.com>
2020-02-23 20:23:31 -0500

    got way to frustrated with alerts, promts, and confirms. scraped the whole idea and rewrote html to include check boxes

--- script.js
+++ script.js
@@ -1,51 +1,31 @@
-var btn = document.querySelector("#generateBtn");
+var char = "abcdefghijklmnopqrstuvwxyz";
+var upr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var num = "0123456789";
+var sym = "!@#$%^&*()_+";
 
-btn.addEventListener("click", function() {
-    alert("Hello and welcome to the Random Password Generator 9000!");
+var charNum = document.getElementById("pwLength");
+var numBox = document.getElementById("numbers");
+var symBox = document.getElementById("symbols");
+var uprBox = document.getElementById("uprCase");
+var submit = document.getElementById("submit");
+var yourPw = document.getElementById("yourPw");
+
+alert("Hello and welcome to the Random Password Generator 9000!");
     
-    alert("Please answer the following questions to set the criteria for your new password!");
-    
-    var pwlength = prompt("How long would you like your Password?", "between 8-128 characters");
-       
-        if (pwlength == null || pwlength < 8) {
-            alert("Sorry, your password needs to be between 8 and 128 characters!");
-        }
-        else {
-            alert("Great we will make your password " + pwlength + " characters long!");
-        }
-        while (pwlength < 8) {
-            var pwlength = prompt("How long would you like your Password?", "between 8-128 characters");
-        }
-    
-    var spechar = confirm("Would you like to include special characters in your password?");
+alert("Please answer the following questions to set the criteria for your new password!");
 
-        if (spechar == true) {
-            alert("Okay! We'll include special characters!");
-        }
-        else {
-            alert("Okay! We won't include any special characters!");
-        }
-    
-    var numbers = confirm("Would you like to include numbers in your password?");
-
-        if (numbers == true) {
-            alert("Okay! We'll include numbers!");
-        }
-        else {
-            alert("Okay! We won't include any numbers!");
-        }
-
-    var uppercase = confirm("Would you like to include uppercase letters in your password?");
-
-        if (uppercase == true) {
-            alert("Okay! We'll include uppercase letters!");
-        }
-        else {
-            alert("Okay! We won't include any uppercase letters!");
-        }
-    
-    alert("Are you ready for your brand new password? Because here it comes!")
-
-        
+submit.addEventListener("click",function(){
+    var characters = char;
+    (numBox.checked) ? characters += num : "";
+    (symBox.checked) ? characters += sym : "";
+    (uprBox.checked) ? characters += upr : "";
+    yourPw.value = password(charNum.value, characters);
 });
 
+function password(l,characters){
+		var pwd = '';
+    for(var i = 0; i<l; i++){
+    		pwd += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return pwd;
+}
